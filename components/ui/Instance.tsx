9f32454f5b070a19bcbda988c7e6003abdbc11a7
wojd0 <wojtekd2000@gmail.com>
2023-11-22 00:24:18 +0100

    fixed buggy lines around action bar when scaled

--- components/ui/Instance.tsx
+++ components/ui/Instance.tsx
@@ -11,10 +11,10 @@ export default function Instance({ id, imageUrl, name }: InstanceProps) {
     const checkImageUrl = () => imageUrl ? `url(${imageUrl})` : "url(/instances/1.png)";
 
     return (
-        <div className="bg-cover bg-no-repeat bg-clip-padding
-            relative border border-stroke-1 text-white hover:scale-[.98] group duration-150 rounded-in w-[512px] h-[256px] flex flex-col place-content-between"
+        <div className="bg-cover bg-no-repeat bg-clip-padding overflow-clip 
+            relative border-stroke-1 text-white hover:scale-[.98] group duration-150 rounded-in w-[512px] h-[256px] flex flex-col place-content-between"
             style={{backgroundImage: `linear-gradient(to bottom,rgba(0,0,0,0),rgba(0,0,0,0.65)), ${checkImageUrl()}`}}>
-            <div className="bg-bg-1 backdrop-blur-main h-fit w-full flex gap-48 px-32 py-16 rounded-[6.5px] opacity-0 group-hover:opacity-100 duration-150">
+            <div className="bg-bg-1 backdrop-blur-main h-fit w-full flex gap-48 px-32 py-16 rounded-b-[6.5px] opacity-0 group-hover:opacity-100 duration-150">
                 <HiPlay className="w-[25px] h-[25px] hover-active-effect hover:text-selected cursor-pointer" />
                 <HiPencil className="w-[25px] h-[25px] hover-active-effect hover:text-selected cursor-pointer" />
                 <HiCog6Tooth className="w-[25px] h-[25px] hover-active-effect hover:text-selected cursor-pointer" />
